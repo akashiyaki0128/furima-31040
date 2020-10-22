312c19af01591c7cbb16f95e9cf1344532965401
akashiyaki0128 <akashiyaki0128@gmail.com>
2020-10-22 17:37:44 +0900

    新規登録機能の実装
    １，application.html.erbのリンク先指定
    ２，new.htmlのform設定　モデル/url
    ３，new.htmlのform設定　ラベル指定
    ４，application controllerにサインアップに必要な要素追記
    ５，バリデーション機能更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname,         presence: true
-         validates :last_name,        presence: true
-         validates :first_name,       presence: true
-         validates :last_name_kana,   presence: true 
-         validates :first_name_kana,  presence: true
-         validates :birth_date,       presence: true
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i                                                      # 大文字・小文字の区別をなくす記述
+    # validates :email, format: { with: VALID_EMAIL_REGEX, message: "is invalid. Include @ character" }, uniqueness: { case_sensitive: false }
+       
+
+  with_options presence: true do
+    validates :nickname         
+    validates :last_name,        format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :first_name,       format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :last_name_kana,   format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."} 
+    validates :first_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :birth_date      
+  end
 end
