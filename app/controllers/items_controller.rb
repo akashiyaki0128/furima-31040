f09241aa25f0cf618055175dd7d1368189d7318d
akashiyaki0128 <akashiyaki0128@gmail.com>
2020-10-24 21:00:53 +0900

    出品画面への遷移
    １，コントローラーにnew/create定義
    ２，ルーティングにリソースで追記
    ３，rails routes で確認
    ４，nullのバリデーション
    ５，出品ページへのリンク先指定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,18 @@
 class ItemsController < ApplicationController
-  def index; end
+  def index
+    
+  end
+
+
+  def new
+  end
+
+
+  def create
+  end
+  
+  
+  
+  
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,15 @@
 class Item < ApplicationRecord
 
+
+  validates  :name, presence: true
+  validates  :note, presence: true
+  validates  :category_id, presence: true
+  validates  :condition_id, presence: true
+  validates  :postage_player_id, presence: true
+  validates  :prefecture_id, presence: true
+  validates  :handling_time_id, presence: true
+  validates  :price, presence: true
+
   belongs_to :user
   has_one_attached :image
 end
