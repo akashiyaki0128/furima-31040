4c346c5f7971f1be3a5d8b3729bdab9b4169118b
akashiyaki0128 <akashiyaki0128@gmail.com>
2020-10-24 18:51:23 +0900

    rubocopの実施

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
   factory :user do
     transient do
-      people{Gimei.name}
+      people { Gimei.name }
     end
-    nickname {Faker::Name.last_name}
-    email {Faker::Internet.free_email}
+    nickname { Faker::Name.last_name }
+    email { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 8)
-    password {password}
-    password_confirmation {password}
-    last_name {people.last.kanji}
-    first_name {people.first.kanji}
-    last_name_kana {people.last.katakana}
-    first_name_kana {people.first.katakana}
-    birth_date {Faker::Date.birthday}
+    password { password }
+    password_confirmation { password }
+    last_name { people.last.kanji }
+    first_name { people.first.kanji }
+    last_name_kana { people.last.katakana }
+    first_name_kana { people.first.katakana }
+    birth_date { Faker::Date.birthday }
   end
 end
