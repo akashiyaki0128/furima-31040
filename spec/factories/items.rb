bb300354e6e8da5dd0a436c9e3915d57fd5b8d48
akashiyaki0128 <akashiyaki0128@gmail.com>
2020-10-26 21:49:46 +0900

    itemモデルのテスト写真あり
    １，public/images/にテスト用のpngを配置
    ２，ファクトリーボットの画像の配置はafter(:build) とすることで、インスタンスがbuildされた後に指定の処理を実行できる、active_storageでつないでいるので、itemのidありきのため
    io: File.openで設定したパスのファイルを保存するphoto.image.attach(io: File.open('public/images/test_image.png'),filename: 'test_image.png')
    ３，ユーザーidを紐付けるためにassociation :userを記述
    ４，bundle exec rspec spec/models/item_spec.rbで確認をしていく

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+    name {"本"}
+    note{"ジャバスクリプト"}
+    category_id{2} 
+    condition_id{2} 
+    postage_player_id{2} 
+    prefecture_id{1}  
+    handling_time_id{2} 
+    price {3000} 
+
+    after(:build) do |photo|
+      photo.image.attach(io: File.open('public/images/test_image.png'),filename: 'test_image.png')
+    end                     
+
   end
 end
