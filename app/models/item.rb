2c77b06d3c2d524ad315947148302105243e94bb
akashiyaki0128 <akashiyaki0128@gmail.com>
2020-10-26 14:14:35 +0900

    出品保存機能の実装
    １，itemモデル　activehashに関する記述修正 Recode →　Record に修正　Name errorがでた　その前にはNo route matchsとエラーが出た
    たぶん　@item=Item.newのタイミングで、ルートOK→コントローラーでItemクラスあれ、おかしいぞ〜？的な
    ２，new actionを新規投稿用に定義
    ３，ストロングパラメーター、マージ作成
    ４，create action 作成
    ５，保存時の条件分岐
    ６，プライス以外のバリデーションとエラーメッセージの設定
    ７，authenticate_user!でindexとshow以外のアクセス制限
    ８，動作チェック　画面/データベース/エラーメッセージ

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecodeExtensions
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :postage_player
@@ -8,16 +8,21 @@ class Item < ApplicationRecord
   belongs_to_active_hash :handling_time
 
   with_options presence: true do
-  validates  :name, 
-  validates  :note, 
-  validates  :category_id, numericality: { other_than: 1 } 
-  validates  :condition_id, numericality: { other_than: 1 } 
-  validates  :postage_player_id, numericality: { other_than: 1 } 
-  validates  :prefecture_id,  numericality: { other_than: 0 } 
-  validates  :handling_time_id, numericality: { other_than: 1 } 
-  validates  :price, 
+  validates  :image
+  validates  :name 
+  validates  :note
+  validates  :category_id, numericality: { other_than: 1 ,message: "must be selected"} 
+  validates  :condition_id, numericality: { other_than: 1 ,message: "must be selected"} 
+  validates  :postage_player_id, numericality: { other_than: 1 ,message: "must be selected"} 
+  validates  :prefecture_id,  numericality: { other_than: 0 ,message: "must be selected"} 
+  validates  :handling_time_id, numericality: { other_than: 1,message: "must be selected" } 
+  validates  :price 
   end
 
   belongs_to :user
   has_one_attached :image
 end
+
+# - 価格の範囲が、¥300~¥9,999,999の間であること
+# - 販売価格は半角数字のみ保存可能であること
+# - 入力された販売価格によって、販売手数料や販売利益の表示が変わること
