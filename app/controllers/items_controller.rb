f4d8270b6ba66587ace300321d13372997f5906c
akashiyaki0128 <akashiyaki0128@gmail.com>
2020-10-27 17:17:38 +0900

    商品編集機能の実装
    １，ルーティング
    ２，コントローラーでedit,update定義
    ３，編集画面へのリンク先を指定
    ４，editビューの編集　newと同一のものを使用
    ５，edit,updateの定義内記述updateの取得するデータの代入先は@itemにする、理由：エラーメッセージのフォームの求めるモデルを@itemで一致させる為
    ６，アクセス制限、プライベートメソッドにensure_current_userを定義し、before_actionでメソッド実行。if文はcurrent_userと@item.userのidを取得して比較するイメージで記述
    ７，rubocop実行済

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :ensure_current_user, only: :edit
 
   def index
     @items = Item.order("created_at desc")
@@ -18,13 +19,36 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   def show
     @item = Item.find(params[:id])
   end
+
   
   private
 
   def item_params
     params.require(:item).permit(:name, :note, :category_id, :condition_id, :postage_player_id, :prefecture_id, :handling_time_id, :image, :price).merge(user_id: current_user.id)
   end
+
+  def ensure_current_user
+    @item = Item.find(params[:id])
+    @user = @item.user_id
+    if current_user.id != @user
+      redirect_to root_path
+    end
+  end
+  
 end
