4da53b324b77a05880bfc827ef39f4d4af76a36b
akashiyaki0128 <akashiyaki0128@gmail.com>
2020-10-28 21:13:05 +0900

    アクセス制限
    １，orderのコントローラーにauthenticate_user!でログインユーザーのみのアクセス制限
    ２,item_ownerで出品者が現ユーザーと同じ場合、urlからでも購入画面に遷移できず、indexに返す設定。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
 
-  before_action :item_data
+  before_action :authenticate_user!
+  before_action :item_owner
   
   def index
     @order = UserOrder.new
@@ -24,17 +25,20 @@ class OrdersController < ApplicationController
     params.require(:user_order).permit(:postal_code, :prefecture_id, :city, :street, :building, :phone_number).merge(item_id: params[:item_id],user_id: current_user.id)
   end
   
-  def item_data
+  def item_owner
     @item = Item.find(params[:item_id])
+    if current_user == @item.user
+      redirect_to root_path
+    end
   end
 end
-
-# - ログアウト状態のユーザーが商品購入ページに遷移しようとすると、ログインページに遷移すること
-# - ログイン状態の出品者が、URLを直接入力して自身の出品した商品購入ページに遷移しようとすると、トップページに遷移すること
-# - ログイン・ログアウトの状態に関わらず、URLを直接入力して売却済み商品の商品購入ページへ遷移しようとすると、トップページに遷移すること
 # - クレジットカード決済ができること
 # - クレジットカードの情報は購入の都度入力させること
 # - クレジットカード情報は必須であり、正しいクレジットカードの情報で無いときは決済できないこと
+# - ログイン・ログアウトの状態に関わらず、URLを直接入力して売却済み商品の商品購入ページへ遷移しようとすると、トップページに遷移すること
+
+# - ログアウト状態のユーザーが商品購入ページに遷移しようとすると、ログインページに遷移すること
+# - ログイン状態の出品者が、URLを直接入力して自身の出品した商品購入ページに遷移しようとすると、トップページに遷移すること
 # - 配送先の住所情報も購入の都度入力させること
 # - 配送先の情報として、郵便番号・都道府県・市区町村・番地・電話番号が必須であること
 # - 郵便番号にはハイフンが必要であること（123-4567となる）
