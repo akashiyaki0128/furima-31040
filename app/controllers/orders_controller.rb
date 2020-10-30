b6e6d8ffb9c0759f45bbdf484e9d67018277680c
akashiyaki0128 <akashiyaki0128@gmail.com>
2020-10-30 10:54:05 +0900

    Payjpにデータ送信設定
    １，gem　payjp追加
    ２，モデルのattr_accessorに:tokenを追加
    ３，ストロングパラメーターに:tokenを追加
    ４，Payjpに送信設定、pay_itemを新たに定義、api_keyの記述、Payjp::Chargeメソッドを使用する。amountは金額が出力される属性を選択する
    ５，Payjpのサイトで送信したデータが確認できればOK

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,7 +10,9 @@ class OrdersController < ApplicationController
   
   def create
     @order = UserOrder.new(order_params)
+    # binding.pry
     if @order.valid?
+      pay_item
       @order.saved
       redirect_to root_path
     else
@@ -22,7 +24,7 @@ class OrdersController < ApplicationController
   
   private
   def order_params
-    params.require(:user_order).permit(:postal_code, :prefecture_id, :city, :street, :building, :phone_number).merge(item_id: params[:item_id],user_id: current_user.id)
+    params.require(:user_order).permit(:postal_code, :prefecture_id, :city, :street, :building, :phone_number).merge(item_id: params[:item_id],user_id: current_user.id,token: params[:token])
   end
   
   def item_owner
@@ -31,6 +33,16 @@ class OrdersController < ApplicationController
       redirect_to root_path
     end
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] #テスト用の秘密鍵
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+  
 end
 # - クレジットカード決済ができること
 # - クレジットカードの情報は購入の都度入力させること
