4af7739a0a9e38d5ce84a799a19c84fc72fa0c34
akashiyaki0128 <akashiyaki0128@gmail.com>
2020-10-30 13:56:24 +0900

    購入機能のテストコード
    １，フォームオブジェクト用のテストモデルを作成する　rails g rspec:model user_order
    ２，ファクトリーボットの作成
    ３，テストの実施

---
+++ spec/factories/user_orders.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :user_order do
+    
+    user_id {"3"}
+    item_id {"5"}
+
+    postal_code {"333-5555"}
+    prefecture_id {"2"}
+    city {"街"}
+    street{"道"}
+    building {"ビル"}
+    phone_number {"05012341234"}
+    token {"tok_abcdefghijk00000000000000000"}
+     
+    
+  end
+end
